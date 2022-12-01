68f8454774610657be127a7c9dcf4ca3b2397b22
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 22:55:36 +0200

    Add project files.

---
+++ charmbar/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/16/2020
+ * Time: 1:29 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace charmbar
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(179, 768);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(1020, 0);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "charmbar";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseLeave += new System.EventHandler(this.MainFormMouseLeave);
+            this.MouseHover += new System.EventHandler(this.MainFormMouseHover);
+            this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ charmbar/MainForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/16/2020
+ * Time: 1:29 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace charmbar
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormMouseHover(object sender, EventArgs e)
+		{
+			while(Left>Screen.PrimaryScreen.WorkingArea.Width-140){
+				Left-=30;
+				Thread.Sleep(50);
+			}
+		}
+		void MainFormMouseLeave(object sender, EventArgs e)
+		{
+			while(Left<Screen.PrimaryScreen.WorkingArea.Width-4){
+				Left+=30;
+				Thread.Sleep(50);
+			}
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
